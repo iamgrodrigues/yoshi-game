73136b4c4232325f49a78e9dc6360f24069a77a7
iamgrodrigues <iamgrodrigues@gmail.com>
2021-02-01 23:36:25 -0300

    wip: Add bowser element

--- script.js
+++ script.js
@@ -35,4 +35,30 @@ function jump() {
   }, 20);
 }
 
+function createBowser() {
+  const bowser = document.createElement('div');
+  let bowserPosition = 1000;
+  let randomTime = Math.random() * 6000;
+
+  bowser.classList.add('bowser');
+  bowser.style.left = 1000 + 'px';
+  background.appendChild(bowser);
+
+  let leftInteval = setInterval(() => {
+    bowserPosition -= 10;
+    bowser.style.left = bowserPosition + 'px';
+
+    if (bowserPosition < -60) {
+      clearInterval(leftInteval);
+      background.removeChild(bowser);
+    } else {
+      bowserPosition -= 10;
+      bowser.style.left = bowserPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createBowser, randomTime);
+}
+
+createBowser();
 document.addEventListener('keyup', handleKeyup);
